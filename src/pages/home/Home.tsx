53f71f39a2c4df46936f478bbb7fcd3f4d9007df
joe061005 <18233368@life.hkbu.edu.hk>
2022-05-14 01:20:07 +0800

    edit the home apge

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import './home.scss'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header type= "home"/>
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
     </div>
   )
 }
